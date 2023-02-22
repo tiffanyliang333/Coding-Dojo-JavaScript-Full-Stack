6faee0c83fb2bef9ade24720d5cb5ff5f46e13a8
Tiffany Liang <110699443+tiffanyliang333@users.noreply.github.com>
2023-02-21 19:35:26 -0500

    updating pokemon react project

--- React/pokemon-api/src/App.js
+++ React/pokemon-api/src/App.js
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    fetch('http://pokeapi.co/api/v2/pokemon?limit=807')
-      .then(response => {
-        return response.json();
-      })
-      .then(response => {
-        setPokemon(response.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+    // fetch('http://pokeapi.co/api/v2/pokemon?limit=807')
+    //   .then(response => {
+    //     return response.json();
+    //   })
+    //   .then(response => {
+    //     setPokemon(response.results);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   })
+    axios.get('http://pokeapi.co/api/v2/pokemon?limit=807')
+      .then((response) =>{console.log(response.data.results);
+        setPokemon(response.data.results);
+    })
   }, []);
 
   return (
     <div className="App">
+      <h1>Pokemon API</h1>
       <ul>
         {
-          pokemon.map((pokemonObject, index) => {
-            return(<li key={index}>{pokemonObject.name}</li>)
+          pokemon.map((pokemonObj, index) => {
+            return(<li key={index}>{pokemonObj.name}</li>)
           })
         }
       </ul>
